f76a897887486d43b7d7e0bcd9e679660810da1f
Jens Thomas <linucks42@gmail.com>
2024-08-04 10:08:39 +0100

    First iteration of sensor

---
+++ mqtt_io/modules/sensor/ds18x20.py
@@ -0,0 +1,51 @@
+"""
+
+DS18X20 Dallas 1-Wire temperature sensor
+
+Example configuration:
+
+sensor_modules:
+  - name: ds18x20
+    module: ds18x20
+    pin: 4
+
+sensor_inputs:
+  - name: temph20
+    module: ds18x20
+    digits: 2
+    interval: 5
+    
+"""
+
+from typing import cast
+from ...types import CerberusSchemaType, ConfigType
+from . import GenericSensor
+
+REQUIREMENTS = ("adafruit_ds18x20", "adafruit_onewire")
+
+
+class Sensor(GenericSensor):
+    """
+    DS18X20 Dallas 1-Wire temperature sensor
+    """
+
+    SENSOR_SCHEMA: CerberusSchemaType = {
+        "pin": dict(
+            type="integer",
+            required=True,
+            empty=False,
+        )
+    }
+
+    def setup_module(self) -> None:
+        # pylint: disable=import-outside-toplevel,import-error
+        import board
+        from adafruit_onewire.bus import OneWireBus
+        from adafruit_ds18x20 import DS18X20
+
+        onewire_bus = OneWireBus(cast(int, self.config["pin"]))
+        self.ds18 = DS18X20(onewire_bus, onewire_bus.scan()[0])  # Assume is only device
+        # self.ds18 = DS18X20(self.onewire_bus)
+
+    def get_value(self, sens_conf: ConfigType) -> float:
+        return cast(float, self.ds18.temperature)
